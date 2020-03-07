c092041f4ba2226f2ba4f6320b07c90fca0b1e06
Bencotti <HanaTwoC@gmail.com>
2020-03-08 00:53:29 +0800

    Add logic to check for directory and file

---
+++ src/main/java/seedu/storage/Storage.java
@@ -0,0 +1,40 @@
+package seedu.storage;
+
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class Storage {
+
+    private static final String WORKING_DIRECTORY = System.getProperty("user.dir");
+    private static final java.nio.file.Path FOLDER_PATH = java.nio.file.Paths.get(WORKING_DIRECTORY, "Duke");
+    private static final java.nio.file.Path FILE_PATH = java.nio.file.Paths.get(WORKING_DIRECTORY, "Duke", "data.txt");
+
+    /**
+     * Locate folder location and check availability
+     * If missing create folder
+     */
+    public static void checkFolderPath() {
+
+        boolean directoryExists = java.nio.file.Files.exists(FOLDER_PATH);
+        try {
+            if (!directoryExists) {
+                Files.createDirectory(FOLDER_PATH);
+                System.out.println("Directory created\n");
+            } else {
+                System.out.println("Directory found\n");
+            }
+        } catch (IOException e) {
+            System.out.println("Error creating folder!\n");
+        }
+    }
+
+    /**
+     * Check whether data file has is present in directory
+     *
+     * @return status of file availability
+     */
+    public static boolean checkFileExists() {
+        boolean fileExists = java.nio.file.Files.exists(FILE_PATH);
+        return fileExists;
+    }
+}

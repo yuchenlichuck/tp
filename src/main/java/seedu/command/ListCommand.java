4cfba3dbb5639f6a9d8ff131d3752598fb76448d
rsanchez-macias <rsanchez-macias@ucmerced.edu>
2020-03-30 02:43:34 -0700

    Add basic list by date functionality

--- src/main/java/seedu/command/ListCommand.java
+++ src/main/java/seedu/command/ListCommand.java
@@ -4,6 +4,7 @@ import seedu.exception.ProjException;
 import java.util.ArrayList;
 import static seedu.common.Constants.TAB;
 import seedu.ui.Ui;
+import seedu.tasks.Task;
 
 public class ListCommand extends Command {
 
@@ -16,6 +17,7 @@ public class ListCommand extends Command {
     private static final int LIST_ALL = 1;
     private static final int LIST_BY_CATEGORY = 2;
     private static final int LIST_BY_DATE = 3;
+    private static final String CLASS_CATEGORY = "CLASS";
 
     public ListCommand(String userCommand) {
         this.userInput = userCommand;
@@ -43,7 +45,7 @@ public class ListCommand extends Command {
             break;
 
         case LIST_BY_DATE:
-
+            getListByDate(listTaskIndex, date, time);
             break;
 
         default:
@@ -54,6 +56,42 @@ public class ListCommand extends Command {
         feedback = getFormattedFeedback(listTaskIndex);
 
         return new CommandResult(feedback);
+    }
+
+    private void getListByDate(ArrayList<Integer> listTaskIndex, String date, String time) {
+        Task dummy = new Task(date, time);
+
+        for (int i = 0; i < taskList.getListSize(); i++) {
+
+            String category = taskList.getTask(i).getCategory().trim();
+
+            String otherDate = "";
+            if (!taskList.getTask(i).getDate().isEmpty()) {
+                otherDate = taskList.getTask(i).getDate().get(0).trim();
+            }
+
+            String otherTime = "";
+            if (!taskList.getTask(i).getTime().isEmpty()) {
+                otherTime = taskList.getTask(i).getTime().get(0).trim();
+            }
+
+            String thisTime = dummy.getTime().get(0).trim();
+            String thisDate = dummy.getDate().get(0).trim();
+
+            if (category.equals(CLASS_CATEGORY)) {
+                continue;
+            }
+
+            if (thisTime.equals(otherTime) && thisDate.equals(otherDate)) {
+                listTaskIndex.add(i);
+                continue;
+            }
+
+            if (thisDate.equals(otherDate) && time.isEmpty()) {
+                listTaskIndex.add(i);
+            }
+        }
+
     }
 
     private void getWholeList(ArrayList<Integer> listTaskIndex) {
@@ -81,6 +119,7 @@ public class ListCommand extends Command {
             throw new ProjException(TAB + "There is no " + category + " in current category.\n"
                     + Ui.DIVIDER);
         }
+
         for (Integer taskIndex : taskList.getCategoryTask(category)) {
             listTaskIndex.add(taskIndex);
         }

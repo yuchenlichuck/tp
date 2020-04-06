44e186a40940d94767b505598c5fc0297e0434f9
yuchenlichuck <yuchenlichuck@126.com>
2020-04-07 02:09:15 +0800

    add listbyDate, add listbyCategory add listbyTime, add listbyDateTime and some listcommand Junit test

--- src/main/java/seedu/command/ListCommand.java
+++ src/main/java/seedu/command/ListCommand.java
@@ -1,12 +1,18 @@
 package seedu.command;
 
+import seedu.calendar.CalendarParser;
 import seedu.exception.ProjException;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.HashSet;
 
 import static seedu.common.Constants.TAB;
 
 
+import seedu.tasks.Class;
 import seedu.tasks.TaskNonclass;
 import seedu.ui.Ui;
 import seedu.tasks.Task;
@@ -38,26 +44,26 @@ public class ListCommand extends Command {
         String date = getDate(userInput).trim();
         String time = getTime(userInput).trim();
 
-        int listCmdSubtype = getCmdSubtype(category, date);
+        int listCmdSubtype = getCmdSubtype(category, date, time);
 
         switch (listCmdSubtype) {
-        case LIST_ALL:
-            getWholeList(listTaskIndex);
-            break;
+            case LIST_ALL:
+                getWholeList(listTaskIndex);
+                break;
 
-        case LIST_BY_CATEGORY:
-            getListByCategory(listTaskIndex, category);
-            break;
-        /*
-        case LIST_BY_DATE:
-            getListByDate(listTaskIndex, date, time);
-            break;
-         */
+            case LIST_BY_CATEGORY:
+                getListByCategory(listTaskIndex, category);
+                break;
 
-        default:
-            // Should not reach here
-            feedback = "[Error][List] No such option to filter";
-            break;
+            case LIST_BY_DATE:
+                getListByDate(listTaskIndex, date, time);
+                break;
+
+
+            default:
+                // Should not reach here
+                feedback = "[Error][List] No such option to filter";
+                break;
         }
 
         feedback = getFormattedFeedback(listTaskIndex);
@@ -66,58 +72,151 @@ public class ListCommand extends Command {
     }
 
     // Shouldn't be called dummy
-    /*
+
     private void getListByDate(ArrayList<Integer> listTaskIndex, String date, String time) {
-        Task dummy = new Task(date, time);
 
-        for (int i = 0; i < taskList.getListSize(); i++) {
-
-            String category = taskList.getTask(i).getCategory().trim();
-
-            String otherDate = "";
-            if (!taskList.getTask(i).getDate().isEmpty()) {
-                otherDate = taskList.getTask(i).getDate().get(0).trim();
+        //only task can do it
+        if (time == null || time.isEmpty()) {
+            String[] dates = date.split("\\s+");
+            //dates input dates
+            HashSet<LocalDate> inputDates = new HashSet<>();
+            for (String d : dates) {
+                LocalDate addedDate = CalendarParser.convertToDate(d);
+                if (addedDate.compareTo(LocalDate.now()) < 0) {
+                    throw new NumberFormatException("Please enter a date that is either today or in the future.");
+                }
+                inputDates.add(addedDate);
             }
 
-            String otherTime = "";
-            if (!taskList.getTask(i).getTime().isEmpty()) {
-                otherTime = taskList.getTask(i).getTime().get(0).trim();
+            int index = -1;
+            int size = inputDates.size();
+            for (Task task : taskList.getList()) {
+                index++;
+                if (task.getCategory().equals("CLASS"))
+                    continue;
+                ArrayList<LocalDate> localDates = task.getDate();
+                int sum = 0;
+
+                for (LocalDate d : localDates) {
+                    if (inputDates.contains(d))
+                        sum++;
+                    if (sum >= size) {
+                        listTaskIndex.add(index);
+                        break;
+                    }
+                }
+                // Populate the date with current date if date is not inputted
+            }
+            return;
+        }
+
+
+        if (date.isEmpty()) {
+            String[] times = time.split("\\s+");
+            ArrayList<LocalTime> startTimes = new ArrayList<>();
+            ArrayList<LocalTime> endTimes = new ArrayList<>();
+            for (String atime : times) {
+                String[] timeRange = atime.split("-");
+                LocalTime startTime = LocalTime.parse(timeRange[0], DateTimeFormatter.ofPattern("HH:mm"));
+                LocalTime endTime = LocalTime.parse(timeRange[1], DateTimeFormatter.ofPattern("HH:mm"));
+                startTimes.add(startTime);
+                endTimes.add(endTime);
             }
 
-            String thisTime = dummy.getTime().get(0).trim();
-            String thisDate = dummy.getDate().get(0).trim();
+            int size = startTimes.size();
 
-            if (category.equals(CLASS_CATEGORY)) {
-                continue;
+            for (int i = 0; i < taskList.getListSize(); i++) {
+                Task task = taskList.getTask(i);
+
+                ArrayList<LocalTime> localTimes = task.getTime();
+
+                int sum = 0;
+                for (int j = 0; j < localTimes.size()/2; j++) {
+                    for (int k = 0; k < size; k++) {
+                        if (localTimes.get(2 * j).equals(startTimes.get(k))
+                                && localTimes.get(2 * j + 1).equals(endTimes.get(k))) {
+                            sum++;
+                        }
+                    }
+                    if (sum >= size) {
+                        listTaskIndex.add(i);
+                        break;
+                    }
+                }
             }
 
-            if (thisTime.equals(otherTime) && thisDate.equals(otherDate)) {
-                listTaskIndex.add(i);
-                continue;
+        }
+
+        //date and time
+        if (!date.isEmpty() && !time.isEmpty()) {
+            String[] dates = date.split("\\s+");
+            String[] times = time.split("\\s+");
+            ArrayList<LocalTime> startTimes = new ArrayList<>();
+            ArrayList<LocalTime> endTimes = new ArrayList<>();
+            ArrayList<LocalDate> dateList = new ArrayList<>();
+
+            for (String atime : times) {
+                String[] timeRange = atime.split("-");
+                LocalTime startTime = LocalTime.parse(timeRange[0], DateTimeFormatter.ofPattern("HH:mm"));
+                LocalTime endTime = LocalTime.parse(timeRange[1], DateTimeFormatter.ofPattern("HH:mm"));
+                startTimes.add(startTime);
+                endTimes.add(endTime);
             }
 
-            if (thisDate.equals(otherDate) && time.isEmpty()) {
-                listTaskIndex.add(i);
+            for (String adate : dates) {
+                dateList.add(LocalDate.parse(adate, DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+            }
+
+            //dates input dates
+            int size = dates.length;
+
+            for (int i = 0; i < taskList.getListSize(); i++) {
+                Task task = taskList.getTask(i);
+                String category = task.getCategory().trim();
+                if (category.equals("CLASS"))
+                    continue;
+
+                ArrayList<LocalTime> localTimes = task.getTime();
+                ArrayList<LocalDate> localDates = task.getDate();
+
+                int sum = 0;
+
+                for (int j = 0; j < localDates.size(); j++) {
+                    for (int k = 0; k < dateList.size(); k++) {
+                        if (localTimes.get(2 * j).equals(startTimes.get(k))
+                                && localTimes.get(2 * j + 1).equals(endTimes.get(k))
+                                && localDates.get(j).equals(dateList.get(k))) {
+                            sum++;
+                        }
+                    }
+
+                    if (sum >= size) {
+                        listTaskIndex.add(i);
+                        break;
+                    }
+                }
             }
         }
 
+
     }
-     */
+
 
     private void getWholeList(ArrayList<Integer> listTaskIndex) {
         for (int i = 0; i < taskList.getListSize(); i++) {
+            //System.out.println(taskList.getTask(i).toString());
             listTaskIndex.add(i);
         }
     }
 
     private String getFormattedFeedback(ArrayList<Integer> listTaskIndex) {
+
         String feedback;
         if (listTaskIndex.size() == 0 || listTaskIndex.size() == 1) {
             feedback = "There are " + listTaskIndex.size() + " task.\n";
         } else {
             feedback = "There are " + listTaskIndex.size() + " tasks.\n";
         }
-
 
         for (int i = 0; i < listTaskIndex.size(); i++) {
             Integer taskIndex = listTaskIndex.get(i);
@@ -126,6 +225,11 @@ public class ListCommand extends Command {
                 feedback += TAB + TAB + (i + TASKLIST_OFFSET) + ". ";
                 feedback += "[" + ((TaskNonclass) task).getStatusIcon() + "] " + task + "\n";
             }
+            if (task instanceof Class) {
+                feedback += TAB + TAB + (i + TASKLIST_OFFSET) + ". ";
+                feedback += "[" + ((Class) task).getStatusIcon() + "] " + task + "\n";
+            }
+
         }
 
         return feedback;
@@ -144,13 +248,13 @@ public class ListCommand extends Command {
         }
     }
 
-    private int getCmdSubtype(String category, String date) {
+    private int getCmdSubtype(String category, String date, String time) {
 
         if (date.isEmpty() && !category.isEmpty()) {
             return LIST_BY_CATEGORY;
         }
 
-        if (!date.isEmpty() && category.isEmpty()) {
+        if (!(date.isEmpty() && time.isEmpty()) && category.isEmpty()) {
             return LIST_BY_DATE;
         }
 
--- src/test/java/seedu/command/ListCommandTest.java
+++ src/test/java/seedu/command/ListCommandTest.java
@@ -105,4 +105,179 @@ class ListCommandTest {
 
         assertEquals(expected, output);
     }
+
+
+    @Test
+    void testListByTime() {
+        TaskList tasks = new TaskList();
+        Ui ui = new Ui();
+        Storage storage = new Storage();
+
+        String output = "";
+        String input = "add n/task t/11:15-13:15 d/2024-02-29";
+
+        Command command = new AddCommand(input);
+        command.setCommandVariables(tasks, storage, ui);
+
+        try {
+            command.execute();
+            command.execute();
+
+        } catch (ProjException e) {
+            assertTrue(false);
+        }
+
+        input = "add n/task t/13:00-15:00 d/2028-02-19";
+
+        command = new AddCommand(input);
+        command.setCommandVariables(tasks, storage, ui);
+        try {
+            command.execute();
+            command.execute();
+            command.execute();
+        } catch (ProjException e) {
+            assertTrue(false);
+        }
+
+        command = new ListCommand("list t/11:15-13:15");
+        command.setCommandVariables(tasks, storage, ui);
+
+        try {
+
+            output = command.execute().getFeedback();
+        } catch (ProjException e) {
+            assertTrue(false);
+        }
+
+        String expected = "There are 2 tasks.\n"
+                + "    1. [N] [TODO] Title: task | 2024-02-29 : 11:15 - 13:15\n"
+                + "    2. [N] [TODO] Title: task | 2024-02-29 : 11:15 - 13:15\n";
+
+        assertEquals(expected, output);
+    }
+
+
+    @Test
+    void testListByDateTime() {
+        TaskList tasks = new TaskList();
+        Ui ui = new Ui();
+        Storage storage = new Storage();
+
+        String output = "";
+        String input = "add n/task t/11:15-13:15 d/2024-02-29";
+
+        Command command = new AddCommand(input);
+        command.setCommandVariables(tasks, storage, ui);
+
+        try {
+            command.execute();
+            command.execute();
+            command.execute();
+        } catch (ProjException e) {
+            assertTrue(false);
+        }
+
+        input = "add n/task t/13:00-15:00 d/2024-02-29";
+
+        command = new AddCommand(input);
+        command.setCommandVariables(tasks, storage, ui);
+        try {
+            command.execute();
+            command.execute();
+            command.execute();
+        } catch (ProjException e) {
+            assertTrue(false);
+        }
+
+        input = "add n/task t/11:15-13:15 d/2024-02-28";
+
+        command = new AddCommand(input);
+        command.setCommandVariables(tasks, storage, ui);
+        try {
+            command.execute();
+            command.execute();
+            command.execute();
+        } catch (ProjException e) {
+            assertTrue(false);
+        }
+
+        command = new ListCommand("list t/11:15-13:15 d/2024-02-29");
+        command.setCommandVariables(tasks, storage, ui);
+
+        try {
+
+            output = command.execute().getFeedback();
+        } catch (ProjException e) {
+            assertTrue(false);
+        }
+
+        String expected = "There are 3 tasks.\n"
+                + "    1. [N] [TODO] Title: task | 2024-02-29 : 11:15 - 13:15\n"
+                + "    2. [N] [TODO] Title: task | 2024-02-29 : 11:15 - 13:15\n"
+                + "    3. [N] [TODO] Title: task | 2024-02-29 : 11:15 - 13:15\n";
+
+        assertEquals(expected, output);
+    }
+
+
+    @Test
+    void testListByCategory() {
+        TaskList tasks = new TaskList();
+        Ui ui = new Ui();
+        Storage storage = new Storage();
+
+        String output = "";
+        String input = "add n/task t/11:15-13:15 d/2024-02-29";
+
+        Command command = new AddCommand(input);
+        command.setCommandVariables(tasks, storage, ui);
+
+        try {
+            command.execute();
+            command.execute();
+            command.execute();
+        } catch (ProjException e) {
+            assertTrue(false);
+        }
+
+        input = "add n/2113 v2.1 t/23:00-24:00 d/2020-05-16 c/deadline";
+
+        command = new AddCommand(input);
+        command.setCommandVariables(tasks, storage, ui);
+        try {
+            command.execute();
+            command.execute();
+        } catch (ProjException e) {
+            assertTrue(false);
+        }
+
+        input = "add t/11:00-12:00 01:00-03:00 n/2113 d/3 4 c/CLASS l/COM2 COM1";
+
+        command = new AddCommand(input);
+        command.setCommandVariables(tasks, storage, ui);
+        try {
+            command.execute();
+            command.execute();
+            command.execute();
+            command.execute();
+        } catch (ProjException e) {
+            assertTrue(false);
+        }
+
+        command = new ListCommand("list c/todo");
+        command.setCommandVariables(tasks, storage, ui);
+
+        try {
+            output = command.execute().getFeedback();
+        } catch (ProjException e) {
+            assertTrue(false);
+        }
+
+        String expected = "There are 3 tasks.\n"
+                + "    1. [N] [TODO] Title: task | 2024-02-29 : 11:15 - 13:15\n"
+                + "    2. [N] [TODO] Title: task | 2024-02-29 : 11:15 - 13:15\n"
+                + "    3. [N] [TODO] Title: task | 2024-02-29 : 11:15 - 13:15\n";
+
+        assertEquals(expected, output);
+    }
 }

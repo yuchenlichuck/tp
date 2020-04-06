c311e23d34f17f3042644f3a69ec8848fe3ae812
rsanchez-macias <rsanchez-macias@ucmerced.edu>
2020-04-06 04:24:45 -0700

    Able to load and save tasks in the text file

--- src/main/java/seedu/command/ListCommand.java
+++ src/main/java/seedu/command/ListCommand.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import static seedu.common.Constants.TAB;
 
 import seedu.tasks.TaskNonclass;
+import seedu.tasks.Class;
 import seedu.ui.Ui;
 import seedu.tasks.Task;
 
@@ -117,6 +118,10 @@ public class ListCommand extends Command {
             if (task instanceof TaskNonclass) {
                 feedback += TAB + TAB + (i + TASKLIST_OFFSET) + ". ";
                 feedback += "[" + ((TaskNonclass) task).getStatusIcon() + "] " + task + "\n";
+            }
+            if (task instanceof Class) {
+                feedback += TAB + TAB + (i + TASKLIST_OFFSET) + ". ";
+                feedback += task + "\n";
             }
         }
 

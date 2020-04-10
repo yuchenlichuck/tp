0a2da6540365bacad3b602fd392192abdd3c6c3d
rsanchez-macias <rsanchez-macias@ucmerced.edu>
2020-04-10 15:45:30 -0700

    Add more tests for line coverage

--- src/test/java/seedu/parser/ParserTest.java
+++ src/test/java/seedu/parser/ParserTest.java
@@ -3,7 +3,6 @@ package seedu.parser;
 import org.junit.jupiter.api.Test;
 
 
-
 import seedu.command.AddCommand;
 import seedu.command.CalendarCommand;
 import seedu.command.ClearCommand;
@@ -12,6 +11,8 @@ import seedu.command.DoneCommand;
 import seedu.command.EditCommand;
 import seedu.command.ExitCommand;
 import seedu.command.FindCommand;
+import seedu.command.FailedCommand;
+import seedu.command.HelpCommand;
 import seedu.command.ListCommand;
 import seedu.command.Command;
 import seedu.common.Constants;
@@ -82,15 +83,161 @@ class ParserTest {
         String input = "delete";
 
         command = Parser.parseCommand(input);
-        boolean isListCommand = command instanceof DeleteCommand;
-        assertTrue(isListCommand);
+        boolean isAddCommand = command instanceof DeleteCommand;
+        assertTrue(isAddCommand);
 
 
         input = "DElEte 2";
         command = Parser.parseCommand(input);
-        isListCommand = command instanceof DeleteCommand;
-        assertTrue(isListCommand);
+        isAddCommand = command instanceof DeleteCommand;
+        assertTrue(isAddCommand);
     }
+
+    @Test
+    public void parseCommand_HelpCommand_createCommand() {
+
+        Command command;
+        String input = "help";
+
+        command = Parser.parseCommand(input);
+        boolean isHelpCommand = command instanceof HelpCommand;
+        assertTrue(isHelpCommand);
+
+
+        input = "HeLP";
+        command = Parser.parseCommand(input);
+        isHelpCommand = command instanceof HelpCommand;
+        assertTrue(isHelpCommand);
+    }
+
+    @Test
+    public void parseCommand_AddCommand_createCommand() {
+
+        Command command;
+        String input = "ADD n/meeting i/project 3";
+
+        command = Parser.parseCommand(input);
+        boolean isAddCommand = command instanceof AddCommand;
+        assertTrue(isAddCommand);
+
+
+        input = "add";
+        command = Parser.parseCommand(input);
+        isAddCommand = command instanceof AddCommand;
+        assertTrue(isAddCommand);
+    }
+
+
+    @Test
+    public void parseCommand_FindCommand_createCommand() {
+
+        Command command;
+        String input = "fINd hello";
+
+        command = Parser.parseCommand(input);
+        boolean isFindCommand = command instanceof FindCommand;
+        assertTrue(isFindCommand);
+
+
+        input = "FIND meeting";
+        command = Parser.parseCommand(input);
+        isFindCommand = command instanceof FindCommand;
+        assertTrue(isFindCommand);
+
+        input = "FINDmeeting";
+        command = Parser.parseCommand(input);
+        isFindCommand = command instanceof FindCommand;
+        assertFalse(isFindCommand);
+    }
+
+    @Test
+    public void parseCommand_EditCommand_createCommand() {
+
+        Command command;
+        String input = "EDit 8 n/meeting i/project 3";
+
+        command = Parser.parseCommand(input);
+        boolean isEditCommand = command instanceof EditCommand;
+        assertTrue(isEditCommand);
+
+
+        input = "ediT";
+        command = Parser.parseCommand(input);
+        isEditCommand = command instanceof EditCommand;
+        assertTrue(isEditCommand);
+    }
+
+    @Test
+    public void parseCommand_ClearCommand_createCommand() {
+
+        Command command;
+        String input = "ClC";
+
+        command = Parser.parseCommand(input);
+        boolean isClearCommand = command instanceof ClearCommand;
+        assertTrue(isClearCommand);
+
+
+        input = "clear";
+        command = Parser.parseCommand(input);
+        isClearCommand = command instanceof ClearCommand;
+        assertFalse(isClearCommand);
+    }
+
+
+    @Test
+    public void parseCommand_DoneCommand_createCommand() {
+
+        Command command;
+        String input = "DoNe 1";
+
+        command = Parser.parseCommand(input);
+        boolean isDoneCommand = command instanceof DoneCommand;
+        assertTrue(isDoneCommand);
+
+
+        input = "donE 3";
+        command = Parser.parseCommand(input);
+        isDoneCommand = command instanceof DoneCommand;
+        assertTrue(isDoneCommand);
+    }
+
+    @Test
+    public void parserCommand_InvalidDoneCommand_returnFailedCommand() {
+
+        Command command;
+        String input = "DoNe";
+
+        command = Parser.parseCommand(input);
+        boolean isFailedCommand = command instanceof FailedCommand;
+        assertTrue(isFailedCommand);
+
+        input = "DoNe 3 2 3";
+
+        command = Parser.parseCommand(input);
+        isFailedCommand = command instanceof FailedCommand;
+        assertTrue(isFailedCommand);
+
+    }
+
+
+    @Test
+    public void parseCommand_ExitCommand_createCommand() {
+
+        Command command;
+        String input = "BYE";
+
+        command = Parser.parseCommand(input);
+        boolean isExitCommand = command instanceof ExitCommand;
+        assertTrue(isExitCommand);
+
+
+        input = "byE";
+        command = Parser.parseCommand(input);
+        isExitCommand = command instanceof ExitCommand;
+        assertTrue(isExitCommand);
+    }
+
 
     @Test
     public void parseCommand_ExitCommandWithParameter_exceptionThrown() {
@@ -113,7 +260,37 @@ class ParserTest {
     }
 
     @Test
-    public void parserCommand_InvalidCommand_errorMessage() {
+    public void parseCommand_CalendarCommand_createCommand() {
+
+        Command command;
+        String input = "calenDAr";
+
+        command = Parser.parseCommand(input);
+        boolean isCalendarCommand = command instanceof CalendarCommand;
+        assertTrue(isCalendarCommand);
+
+
+        input = "CALENDAR 14";
+        command = Parser.parseCommand(input);
+        isCalendarCommand = command instanceof CalendarCommand;
+        assertTrue(isCalendarCommand);
+
+    }
+
+    @Test
+    public void parseCommand_InvalidCalendarCommand_returnFailedCommand() {
+
+        Command command;
+        String input = "calenDAr 1 2 3 4 5";
+
+        command = Parser.parseCommand(input);
+        boolean isFailedCommand = command instanceof FailedCommand;
+        assertTrue(isFailedCommand);
+
+    }
+
+    @Test
+    public void parseCommand_InvalidCommand_errorMessage() {
 
         Command command;
         String input = "exit";
